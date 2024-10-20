34675bd9808b5fb1ff417259a759e73a7f89b372
amitai-debiche <amivids603+gh@gmail.com>
2024-10-20 15:33:44 -0500

    update; split up and added script to send data to kafka stream

--- weekly_data/download_csv_convert_avro.py
+++ weekly_data/download_csv_convert_avro.py
@@ -1,5 +1,8 @@
 import requests
 import pandas as pd
+import fastavro
+import json
+
 
 def download_large_file(url, filename):
     """
@@ -59,9 +62,67 @@ def process_large_csv(filename):
 
     print(f"Total rows processed: {total_rows_processed}")
 
-if __name__ == "__main__":
-#    csv_url = 'https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_covid19/weekly_housing_market_data_most_recent.tsv000'
-    local_filename = 'redfin_weekly_data.csv'
+def convert_csv_to_avro(csv_filename, avro_filename, avro_schema):
+    """
+    Convert CSV file to an Avro file
 
-#    download_large_file(csv_url, local_filename)
-    process_large_csv(local_filename)
+    :param csv_filename: The path to the CSV file.
+    :type csv_filename: str
+    :param avro_filename: The path to save the Avro file.
+    :type avro_filename: str
+    :param avro_schema: The Avro schema for the file.
+    :type avro_schema: dict 
+    """
+    try:
+        df = pd.read_csv(csv_filename, sep='\t', low_memory=False)
+
+        records = df.to_dict(orient='records')
+
+        with open(avro_filename, 'wb') as avro_file:
+            fastavro.writer(avro_file, avro_schema, records)
+
+    except Exception as e:
+        print(f"Error occured during CSV to Avro conversion: {e}")
+
+def read_first_n_records(avro_file, n):
+    """
+    Reads the first n records from an Avro file and prints them.
+
+    :param avro_file: The path to the Avro file.
+    :type avro_file: str
+    :param n: The number of records to read.
+    :type n: int
+    """
+    try:
+        with open(avro_file, 'rb') as f:
+            reader = fastavro.reader(f)
+            for i, record in enumerate(reader):
+                if i >= n:
+                    break
+                print(record)
+    except Exception as e:
+        print(f"Error reading Avro file: {e}")
+
+def load_schema_from_file(schema_path):
+
+    try:
+        with open(schema_path, 'r') as schema_file:
+            schema = json.load(schema_file)
+        return schema
+    except Exception as e:
+        print(f"Error loading schema from file: {e}")
+        return None
+
+if __name__ == "__main__":
+    csv_url = 'https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_covid19/weekly_housing_market_data_most_recent.tsv000'
+    local_filename = 'redfin_weekly_data.csv'
+    avro_filename = 'redfin_weekly_data.avro'
+    schema_filename = 'weekly_data_schema.avsc'
+
+    avro_schema = load_schema_from_file(schema_filename)
+
+    download_large_file(csv_url, local_filename)
+    #process_large_csv(local_filename)
+    #read_first_n_records(avro_filename, 50)
+    if avro_schema:
+        convert_csv_to_avro(local_filename, avro_filename, avro_schema)
---
+++ weekly_data/send_to_kafka.py
@@ -0,0 +1,84 @@
+import fastavro
+import os
+
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from uuid import uuid4
+
+def delivery_report(err, msg):
+    """
+    Reports the failure or success of a message delivery.
+
+    :param err: The error that occured on none on success.
+    :type: KafkaError
+    :param msg: The message that was produced or failed
+    :type: Message
+
+    
+    Note:
+        In the delivery report callback the Message.key() and Message.value()
+        will be the binary format as encoded by any configured Serializers and
+        not the same object that was passed to produce().
+        need bound callback or lambda where you pass
+        the objects along.
+    """
+
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+def send_avro_to_kafka(avro_file, schema_registry_url, kafka_topic, kafka_broker, schema_file):
+    """
+    Send Avro data from an Avro file to Kafka.
+
+    :param avro_file: Path to the Avro file.
+    :param schema_registry_url: URL of the schema registry.
+    :param kafka_topic: Kafka topic to send messages to.
+    :param kafka_broker: Kafka broker URL.
+    :param schema_file: Path to the Avro schema file.
+    """
+    # Load Avro schema
+
+    path = os.path.realpath(os.path.dirname(__file__))
+    with open(f"{path}/avro/{schema_file}") as f:
+        schema_str = f.read()
+
+    schema_registry_config = {'url': schema_registry_url}
+    schema_registry_client = SchemaRegistryClient(schema_registry_config)
+
+
+    string_serializer = StringSerializer('utf-8')
+
+    producer_conf = {'bootstrap.servers': kafka_broker }
+    producer = Producer(producer_conf)
+
+    print("Producing records to topic")
+
+    # Initialize Kafka Avro Producer
+    # Open and read the Avro file
+    with open(avro_file, 'rb') as f:
+        reader = fastavro.reader(f)
+        for record in reader:
+            try:
+                # Send each record to Kafka
+                producer.produce(topic=kafka_topic,
+                                 key=string_serializer(str(uuid4())),
+                                 value=record,
+                                 on_delivery=delivery_report)
+                producer.flush()
+                print(f"Record sent to topic {kafka_topic}: {record}")
+            except Exception as e:
+                print(f"Failed to send record to Kafka: {e}")
+
+if __name__ == "__main__":
+    avro_file_path = 'redfin_weekly_data.avro'
+    schema_file_path = 'weekly_data_schema.avsc' 
+    kafka_topic = 'your_topic_name' # Kafka to topic that we want to send records to 
+    kafka_broker = 'localhost:9092'  # Kafka broker URL when setup
+    schema_registry_url = 'http://localhost:8081'  # Schema registry 
+
+    send_avro_to_kafka(avro_file_path, schema_registry_url, kafka_topic, kafka_broker, schema_file_path)
